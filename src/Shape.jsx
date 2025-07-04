763ba3007134d88ac75d4140f25e49e6ee093991
Rathish <rathish@Rathishs-MacBook-Air.local>
2025-07-05 02:05:46 +0530

    feat: Add drag feature to the shape component

--- src/Shape.jsx
+++ src/Shape.jsx
@@ -1,9 +1,20 @@
+import { useDrag } from 'react-dnd';
 import './Shape.css';
 
 const Shape = ({ id, type }) => {
+    
+  // Enable drag feature for shapes and fade out the shape while being dragged  
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'shape',
+    item: { id },
+    collect: (monitor) => ({ isDragging: !!monitor.isDragging() }),
+  }));
+
   return (
     <div
+      ref={drag}
       className={`shape ${type}`}
+      style={{ opacity: isDragging ? 0.5 : 1 }}
     ></div>
   );
 };
